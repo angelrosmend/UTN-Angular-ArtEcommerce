7c391c3ed5b9a868d16916526c2090d94bd261af
angelrosmend <angelrosmend@gmail.com>
2020-06-20 18:22:10 -0300

    Version entrega para la UTN

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tp2'`, () => {
+  it(`should have as title 'UTN ANGULAR'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('tp2');
